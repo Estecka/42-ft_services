6d6405aa6a6b8a2d4f3138cbae61cbc0a54f1871
abaur <abaur@student.42.fr>
2020-08-25 15:41:56 +0200

    Custom image pod.
    Capable of running a custom docker image in a minikube pod.

--- setup.sh
+++ setup.sh
@@ -4,7 +4,24 @@
 if [ "$1" = "prelude" ]
 then
 	sudo usermod -aG docker $(whoami);
+	minikube start --vm-driver=docker
 	exit;
+elif [ "$1" = "build" ]
+then
+	eval $(minikube docker-env)
+	docker build -t ftnginx srcs/ft_nginx/
+	exit
+elif [ "$1" = "run" ]
+then
+	kubectl run ftnginx --image-pull-policy=Never --image ftnginx
+elif [ "$1" = "bake" ]
+then
+	kubectl get pod ftnginx -o yaml > srcs/ft_nginx.yaml
+elif [ "$1" = "apply" ]
+then
+	kubectl apply -f srcs/ft_nginx.yaml
+elif [ "$1" = "delete" ]
+then
+	kubectl delete pod ftnginx
 fi
 
-minikube start --vm-driver=docker
