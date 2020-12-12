bc5381cf8cfb8afea9b0d1c37a04928ffc314611
abaur <abaur@student.42.fr>
2020-12-12 12:29:50 +0100

    Installed php on ftnginx.

---
+++ srcs/ftnginx/files/usr/share/nginx/html/index.php
@@ -0,0 +1,27 @@
+<?php
+$host = $_SERVER['HTTP_HOST'];
+?>
+<html>
+	<head>
+		<title>Abaur's ft_service</title>
+		<meta name="author" content="abaur"/>
+	</head>
+	<body>
+		<h1>ft_services</h1>
+		<h2>by abaur</h2>
+		<p>
+			<span title=":3c">Hellow World.</span>
+		</p>
+		<p>
+			<b>Host IP</b>: <?=$host?>
+		</p>
+		<ul>
+			<li>
+				Nginx:
+				[<a href="http://<?=$host?>">http</a>]
+				[<a href="https://<?=$host?>">https</a>]
+			</li>
+			<li><a href="http://<?=$host?>:9090">Kubernetes Dashboard</a></li>
+		</ul>
+	</body>
+</html>
