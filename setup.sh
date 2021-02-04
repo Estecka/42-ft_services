dc30365eec96f6b8fb354369cea843ebbc0ff68f
abaur <abaur@student.42.fr>
2021-02-04 16:29:18 +0100

    Fixed auto-usermod

--- setup.sh
+++ setup.sh
@@ -2,8 +2,18 @@
 
 cd $(dirname $BASH_SOURCE) || exit 1
 
-sudo usermod -aG docker $(whoami) \
-&& minikube delete \
+docker ps >/dev/null 2>&1 \
+||	if [ "$1" = "--no-loop" ] 
+	then
+		echo "Failed to acquire docker permissions.";
+		exit 1;
+    else
+        sudo usermod -aG docker $USER \
+        && newgrp docker <<< "$BASH_SOURCE --no-loop"; 
+        exit;
+	fi
+
+minikube delete \
 && minikube start \
 	--vm-driver=docker \
 	--extra-config=kubelet.eviction-hard="nodefs.available<1Gi" \
